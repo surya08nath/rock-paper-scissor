a97ccb969e8b7bef8ef79b46c66f1afa03c8c497
Suryanath G <121388374+surya08nath@users.noreply.github.com>
2025-01-05 12:18:38 +0530

    Computer choice is created

---
+++ game.js
@@ -0,0 +1,8 @@
+function getComputerChoice() {
+    const choices = ["rock", "paper", "scissors"];
+    const randomIndex = Math.floor(Math.random() * choices.length);
+    return choices[randomIndex];
+  }
+  
+  console.log(getComputerChoice());
+  
